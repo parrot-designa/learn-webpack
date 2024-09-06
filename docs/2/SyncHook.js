f600e609ee5013a2214c76a10e9c3c6f81e3e14a
wujiabao <wujiabao@leateckgroup.com>
2024-09-06 18:01:18 +0800

    - SyncHook 解析

---
+++ docs/2/SyncHook.js
@@ -0,0 +1,28 @@
+// 同步的钩子
+const { SyncHook } = require('tapable');
+
+class SyncDemo {
+    constructor(){
+        // 吃早餐
+        this.eatBreakfast = new SyncHook();
+    }
+}
+
+let asyncInstance = new SyncDemo();
+
+asyncInstance.eatBreakfast.tap('吃早餐',()=>{
+    console.log('吃第一次早餐'); 
+    let price = 0;
+    for(let i = 0; i < 1000000000;i++){
+        price += 1;
+    }
+    console.log('花了'+price + '元'); 
+})
+asyncInstance.eatBreakfast.tap('吃早餐',()=>{
+    console.log('吃第二次早餐');
+})
+asyncInstance.eatBreakfast.tap('吃早餐',()=>{
+    console.log('吃第三次早餐');
+})
+
+asyncInstance.eatBreakfast.call();
